74fbcc6f3b82d6b52b707bcc28e15e0f84e31d98
andrewkos <andrew_kos@hotmail.com>
2014-12-06 17:08:01 +0000

    TightProviders
    Added type providers

---
+++ SummonersGift/FetchFunctions.fs
@@ -0,0 +1,24 @@
+﻿namespace SummonersGift
+
+open FSharp.Data 
+open SummonersGift.Keys
+
+module Constants = 
+
+    
+
+module FetchFunctions =
+
+    type Game1_3 = JsonProvider<"..\ExampleJSON\game-v1.3.JSON">
+    type League2_5 = JsonProvider<"..\ExampleJSON\league-v2.5.JSON">
+    type MatchHistory2_2 = JsonProvider<"..\ExampleJSON\matchhistory-v2.2.JSON">
+    type Match2_2 = JsonProvider<"..\ExampleJSON\match-v2.2.JSON">
+    type Stats1_3_Ranked = JsonProvider<"..\ExampleJSON\stats-v1.3_ranked.JSON">
+    type Stats1_3_Summary = JsonProvider<"..\ExampleJSON\stats-v1.3_summary.JSON">
+    type Summoner1_4 = JsonProvider<"..\ExampleJSON\summoner-v1.4.JSON">
+
+
+    let getSummoner(region:string)(summonerName:string)=
+
+        "https://" + region + ".api.pvp.net/api/lol/" + region + "/v1.4/summoner/by-name/" + summonerName.Replace(" ","%20") + "?api_key=" + lolApiKey
+            |> Summoner1_4.Load
--- SummonersGift/KosScripting.fsx
+++ SummonersGift/KosScripting.fsx
@@ -1,1 +1,9 @@
 ﻿
+#r @"C:\Users\Andrew\Documents\GitHub\SummonersGift\packages\FSharp.Data.2.1.0\lib\net40\FSharp.Data.dll"
+open FSharp.Data 
+
+type SomeProvider = JsonProvider<"..\ExampleJSON\game-v1.3.JSON">
+
+let game = SomeProvider.Load(@"C:\Users\Andrew\Documents\GitHub\SummonersGift\ExampleJSON\game-v1.3.json")
+
+game.Games.[0].ChampionId
