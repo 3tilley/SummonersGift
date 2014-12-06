124c714fb91b7ec8decf602d462c763f12077937
andrewkos <andrew_kos@hotmail.com>
2014-12-06 18:19:42 +0000

    NewtonSoft JSON deserialiser
    Added better JSON handling for summoners

--- SummonersGift/FetchFunctions.fs
+++ SummonersGift/FetchFunctions.fs
@@ -3,7 +3,13 @@
 open FSharp.Data 
 open SummonersGift.Keys
 
+module DataSource =
+
+    let wc = new System.Net.WebClient()
+
 module FetchFunctions =
+
+    open DataSource
 
     type Game1_3 = JsonProvider<"..\ExampleJSON\game-v1.3.JSON">
     type League2_5 = JsonProvider<"..\ExampleJSON\league-v2.5.JSON">
@@ -13,8 +19,20 @@ module FetchFunctions =
     type Stats1_3_Summary = JsonProvider<"..\ExampleJSON\stats-v1.3_summary.JSON">
     type Summoner1_4 = JsonProvider<"..\ExampleJSON\summoner-v1.4.JSON">
 
+    type Summoner = {id:int;name:string;profileIconId:int;summonerLevel:int;revisionDate:int64}
 
-    let getSummoner (region:string) (summonerName:string) =
+//    This function isn't great.
+//    let getSummoner (region:string) (summonerName:string) =
+//
+//        "https://" + region + ".api.pvp.net/api/lol/" + region + "/v1.4/summoner/by-name/" + summonerName.Replace(" ","%20") + "?api_key=" + lolApiKey
+//            |> Summoner1_4.Load
 
-        "https://" + region + ".api.pvp.net/api/lol/" + region + "/v1.4/summoner/by-name/" + summonerName.Replace(" ","%20") + "?api_key=" + lolApiKey
-            |> Summoner1_4.Load
+    let getSummoner (region:string) (summonerNames:string[]) = 
+
+        let summoners = (String.concat "," summonerNames).Replace(" ","%20") 
+        let query = "https://" + region + ".api.pvp.net/api/lol/" + region + "/v1.4/summoner/by-name/" + summoners + "?api_key=" + lolApiKey
+        let result = wc.DownloadString query
+        let dictType = System.Collections.Generic.Dictionary<string,Summoner>().GetType()
+        let deserialised = Newtonsoft.Json.JsonConvert.DeserializeObject(result,dictType)
+        deserialised :?> System.Collections.Generic.Dictionary<string,Summoner>
+        
--- SummonersGift/KosScripting.fsx
+++ SummonersGift/KosScripting.fsx
@@ -4,9 +4,13 @@
 #r @"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5\System.Numerics.dll"
 #r @"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5\System.Web.dll"
 #r @"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5\System.Xml.Linq.dll"
+#r @"C:\Users\Andrew\Documents\GitHub\SummonersGift\packages\Newtonsoft.Json.6.0.6\lib\net45\Newtonsoft.Json.dll"
 
 #load "ApiKeys.fs"
 #load "FetchFunctions.fs"
 
-let kf = SummonersGift.FetchFunctions.getSummoner "euw" "Proheme"
-kf.JsonValue
+let kf = SummonersGift.FetchFunctions.getSummoner "euw" [|"Proheme";"Khaos Force"|]
+kf.["proheme"]
+
+
+
