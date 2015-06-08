607726ac3efe5cb5cfc7128392a8e3fa02e3cbaa
Max Tilley <3tilley@googlemail.com>
2015-06-08 22:08:31 +0100

    more debugging

---
+++ SummonersGift.Web/Common/ExceptionHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Filters;
+using System.Web.Http;
+using System.Net;
+using System.Net.Http;
+using System.Diagnostics;
+
+namespace SummonersGift.Web.Common
+{
+    public class ExceptionHandlerAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext actionExecutedContext)
+        {
+            Trace.TraceError(actionExecutedContext.Exception.ToString());
+
+            throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.InternalServerError)
+            {
+                Content = new StringContent("An error occurred, please try again or contact the administrator."),
+                ReasonPhrase = "Critical Exception"
+            });
+        }
+    }
+}
